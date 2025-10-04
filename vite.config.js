b2a17f4322f1a4dbc30a19142e5813d1d242468e
Amit Kumar Sharma <hemantlife80@gmail.com>
2025-10-04 08:15:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.ts
+// vite.config.ts (सही कोड)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NovaStudio-AI/', // <-- इसे यहाँ बदलें!
+  // base वाली लाइन को हमने यहाँ से हटा दिया है
 })
